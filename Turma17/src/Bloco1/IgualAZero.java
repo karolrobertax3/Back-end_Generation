1f9f965ad6bf97e586f7b236976c547f5350cb4a
karolrobertax3 <karolroberta92@hotmail.com>
2021-02-26 20:04:16 -0300

    Exercícios de Repetição do 1 ao 6, dia 260/02/2021

--- Turma17/src/Bloco1/IgualAZero.java
+++ Turma17/src/Bloco1/IgualAZero.java
@@ -3,11 +3,22 @@ import java.util.Scanner;
 
 public class IgualAZero {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// EXERCÍCIO 5 DE REPETIÇÃO 26/02/2021
-		int numeros, soma;
+		int numeros, soma=0;
 		Scanner sc = new Scanner(System.in);
+		
+		do 
+		{			
+			System.out.println("Insira os números: ");
+			numeros = sc.nextInt();
+			soma = soma + numeros;
+						
+		}while(numeros!=0);
 
-	}
+		
+		System.out.println("Total: " + soma);
+	}	
 
 }
--- Turma17/src/Bloco1/PesquisaCaracteristica.java
+++ Turma17/src/Bloco1/PesquisaCaracteristica.java
@@ -3,21 +3,64 @@ package Bloco1;
 import java.util.Scanner;
 
 public class PesquisaCaracteristica {
-
+		//EXERCÍCIO 4 LAÇOS DE REPETIÇÕES 26/02/2021
+	
 	public static void main(String[] args) {
-		int x,idade, sexo, fp, contpc=0, contmn, contha=0, contoc, contpn40, contpc18
+		int i=1,idade, sexo, fatorPsicologico, countpessoascalmas=0, countmulhernervosa=0, counthomemagressivo=0, countoutroscalmos=0, countnervosa40=0, countcalma18=0;
 		Scanner sc = new Scanner(System.in);
 		
-		while(x<=6) {
+		while(i<=2 ) 
+			{
 			System.out.println("Entre com sua idade: ");
 			idade = sc.nextInt();
-		while()
-			System.out.println("Entre com seu gênero: 1-- Feminino -- 2 Masculino -- 3 Outros ");
-			idade = sc.nextInt();
-		}
 		
-		System.out.println("Entre com seu fator psicológico: 1-- Calma -- 2 Nervosa -- 3 Agressivo" );
+			System.out.println("Entre com seu gênero: 1-- Feminino -- 2 Masculino -- 3 Outros ");
+			sexo = sc.nextInt();
+			
+			System.out.println("Entre com o seu fator psicológico: 1 -- Calmo 2 -- Nervoso 3 -- Agressivo");
+			fatorPsicologico = sc.nextInt();
+			
+			if(fatorPsicologico==1)
+			{
+				countpessoascalmas++;
+			}
+			
+			if(sexo==1 && fatorPsicologico ==2)
+			{
+				countmulhernervosa++;
+			}
+			
+			if(sexo==2 && fatorPsicologico ==3)
+			{
+				counthomemagressivo++;
+			}
+			
+			if(sexo==3 && fatorPsicologico==1)
+			{
+				countoutroscalmos++;
+			}
+			
+			if(fatorPsicologico ==2 && idade>40)
+			{
+				countnervosa40++;
+			}
+			
+			if(fatorPsicologico ==1 && idade<18)
+			{
+				countcalma18++;
+
+			}
+		
+		i++;
 	}
+	
+		System.out.println("\nPessoas calmas: "+ countpessoascalmas);
+		System.out.println("\nMulheres nervosas: "+ countmulhernervosa);
+		System.out.println("\nHomens Agressivos: "+ counthomemagressivo);
+		System.out.println("\nOutros calmos: "+ countoutroscalmos);
+		System.out.println("\nNervosos com mais de 40 anos: "+ countnervosa40);
+		System.out.println("\nCalmas com menos de 18 anos: "+ countcalma18);
+}
 
 }
  
