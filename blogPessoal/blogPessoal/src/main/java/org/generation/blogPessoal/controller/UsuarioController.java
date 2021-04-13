d7921ad5c32aa80d843a46aed4d990b3c6684170
karolrobertax3 <karolroberta92@hotmail.com>
2021-04-12 22:26:18 -0300

    Implementado o swagger e deploy

--- blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package org.generation.blogPessoal.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +24,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
 	private @Autowired UsuarioService services;
+	
+	@GetMapping("/busca")
+	public ResponseEntity<List<Usuario>> getAll() {
+		return new ResponseEntity<List<Usuario>>(services.getAll(), HttpStatus.OK);
+	}
 	
 	@PostMapping("/cadastrar")
 	public ResponseEntity<Usuario> cadastro(@Valid @RequestBody Usuario usuario){
--- blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -1,6 +1,7 @@
 package org.generation.blogPessoal.service;
 
 import java.nio.charset.Charset;
+import java.util.List;
 import java.util.Optional;
 
 import org.apache.commons.codec.binary.Base64;
@@ -19,30 +20,43 @@ public class UsuarioService {
 	
 	public Usuario CadastrarUsuario(Usuario usuario) 
 	{
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		Optional<Usuario> usuarioExistente = repository.findByUsuario(usuario.getUsuario());
+		if (usuarioExistente.isPresent()) {
+			return null;
+		} else {
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			
+			String senhaEncoder = encoder.encode(usuario.getSenha());
+			usuario.setSenha(senhaEncoder);
+			return repository.save(usuario);
+		}
 		
-		String senhaEncoder = encoder.encode(usuario.getSenha());
-		usuario.setSenha(senhaEncoder);
-		return repository.save(usuario);
 		
 		}
 	public Optional<UserLogin> logar(Optional<UserLogin> user)
 	{
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
+		Optional<Usuario> usuarioPresente = repository.findByUsuario(user.get().getUsuario());
+		if(usuarioPresente.isPresent()) {
 		
-		if(encoder.matches(user.get().getSenha(), usuario.get().getSenha()))
+		if(encoder.matches(user.get().getSenha(), usuarioPresente.get().getSenha()))
 		{
 			String auth = user.get().getUsuario() + ":" + user.get().getSenha();
 			byte[] encodeAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
 			String authHeader = "Basic " + new String(encodeAuth);
 			
 			user.get().setToken(authHeader);
-			user.get().setNome(usuario.get().getNome());
-			user.get().setSenha(usuario.get().getSenha());
+			user.get().setNome(usuarioPresente.get().getNome());
+			user.get().setSenha(usuarioPresente.get().getSenha());
 			
 			return user;
 			}
+		
+		}
 		return null;
+	}
+	
+	public List<Usuario> getAll(){
+		return repository.findAll();
 	}
 }
